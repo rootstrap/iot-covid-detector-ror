3206f2b7054965e5087d09a76206f3e812790779
Guillermo Leopold <guillermo@192.168.68.112>
2021-07-21 16:22:32 -0300

    clean unnecesary files

--- spec/decorators/user_decorator_spec.rb
+++
@@ -1,8 +0,0 @@
-describe UserDecorator do
-  let(:user) { create(:user) }
-  let(:decorated_user) { user.decorate }
-
-  it 'returns same email' do
-    expect(decorated_user.email).to eq(user.email)
-  end
-end
