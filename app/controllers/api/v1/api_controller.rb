3de9916e479752d829c982d93871b881914818aa
Juan Manuel Ferreira <juanmferreira93@gmail.com>
2021-08-13 09:19:17 -0300

    Remove olds specs

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,12 +3,10 @@ module Api
     class ApiController < ActionController::API
       include Api::Concerns::ActAsApiRequest
       include Pundit
-      include DeviseTokenAuth::Concerns::SetUserByToken
 
       after_action :verify_authorized, except: :index
       after_action :verify_policy_scoped, only: :index
 
-      before_action :authenticate_user!, except: :status
       skip_after_action :verify_authorized, only: :status
 
       rescue_from ActiveRecord::RecordNotFound,        with: :render_not_found
--- spec/policies/admin/application_policy_spec.rb
+++ spec/policies/admin/application_policy_spec.rb
@@ -9,20 +9,12 @@ describe Admin::ApplicationPolicy do
         expect(subject).to permit(admin, admin)
       end
     end
-
-    context 'when is a user' do
-      let(:user) { build(:user) }
-
-      it 'denies access' do
-        expect(subject).not_to permit(user, user)
-      end
-    end
   end
 
   describe 'scope' do
     let(:admin) { create(:admin_user) }
     let(:mock_model) { double('MockModel', all: true) }
-    subject { ApplicationPolicy::Scope.new(admin, mock_model).resolve }
+    subject { Admin::ApplicationPolicy::Scope.new(admin, mock_model).resolve }
 
     it 'shows all models' do
       expect(subject).to be(true)
